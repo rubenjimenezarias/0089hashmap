55f810e615d50fe4f58db3fa05e9e58e407b3c33
rubenjimenezarias <rubenjimenezarias@gmail.com>
2015-02-25 11:50:27 +0100

    primer commit con metodos enterNumber y lookupNumber

---
+++ AgendaTelefonica.java
@@ -0,0 +1,40 @@
+import java.util.HashMap;
+/**
+ * Write a description of class AgendaTelefonica here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+public class AgendaTelefonica
+{
+    // instance variables - replace the example below with your own
+    private HashMap <String,String> agenda;
+
+    /**
+     * Constructor for objects of class AgendaTelefonica
+     */
+    public AgendaTelefonica()
+    {
+        // initialise instance variables
+        agenda = new HashMap<>();
+    }
+
+    /**
+     * Introduce un nombre con un numero a la agenda
+     * 
+     * @name  clave nombre
+     * @number numero de telefono 
+     */
+    public void enterNumber(String name, String number)
+    {
+        agenda.put(name,number);
+    }
+    
+    /**
+     * Devuelve el numero de telefon de un nombre que le indiques
+     */
+    public void lookupNumber(String name)
+    {
+        agenda.get(name);
+    }
+}
